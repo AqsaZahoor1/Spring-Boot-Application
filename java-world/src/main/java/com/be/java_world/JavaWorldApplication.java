542b8fad94b6010367f5df3f63ee9ed65709a05b
Aqsa Zahoor1 <aqsazahoor555@gmail.com>
2025-04-15 03:58:21 +0500

    Initialized git

---
+++ java-world/src/main/java/com/be/java_world/JavaWorldApplication.java
@@ -0,0 +1,13 @@
+package com.be.java_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/be/java_world/JavaWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.java_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
